21a8da64c365902e07d9f619742b5fa8a9079d19
Hirotoshi Yoshitaka <tyounan.moti@gmail.com>
2019-06-23 14:43:23 +0900

    Fixed corrupt editor rendering

--- Plugin/UnityLiveShader/entrypoint.cpp
+++ Plugin/UnityLiveShader/entrypoint.cpp
@@ -197,11 +197,13 @@ private:
     D3D11_INPUT_ELEMENT_DESC input_element_desc[] =
     {
       { "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
-      { "COLOR", 0, DXGI_FORMAT_R8G8B8A8_UNORM, 0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+      { "NORMAL", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+      { "COLOR", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, 24, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+      { "UV", 0, DXGI_FORMAT_R32G32_FLOAT, 0, 40, D3D11_INPUT_PER_VERTEX_DATA, 0 },
     };
     hr = d3d11_device->CreateInputLayout(
       input_element_desc,
-      2,
+      4,
       vertex_shader_bytecode->GetBufferPointer(),
       vertex_shader_bytecode->GetBufferSize(),
       &input_layout
